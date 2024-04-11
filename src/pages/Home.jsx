043d26d0fac3a114c6916d1ece48924c254349d5
Matheus Augusto <matheusaugus233@gmail.com>
2024-04-10 21:40:29 -0300

    feat: :lipstick: Add Home Style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import MovieCard from "../components/MovieCard";
 
+import './MoviesGrid.css';
+
 
 const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
