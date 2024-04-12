556e02c5f1da2e79d48b781b61661454b6410003
Matheus Augusto <matheusaugus233@gmail.com>
2024-04-12 18:51:49 -0300

    feat: :sparkles: Search Function

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,28 @@
-import { Link } from 'react-router-dom'
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom'
 import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
 
 import './Navbar.css';
 
 export const Navbar = () => {
-    return (
+    const [search, setSearch] = useState("");
+    const navigate = useNavigate();
 
+    const hadleSubmit = (e) => {
+        e.preventDefault();
+        if (!search) return
+        navigate(`/search?q=${search}`);
+        setSearch("");
+    }
+
+
+    return (
         <nav id="navbar">
             <h2>
                 <Link to="/"><BiCameraMovie />MoviesLib</Link>
             </h2>
-            <form>
-                <input type="text" placeholder='Busque um filme' />
+            <form onSubmit={hadleSubmit}>
+                <input type="text" placeholder='Busque um filme' onChange={(e) => setSearch(e.target.value)} value={search} />
                 <button type='submit'>
                     <BiSearchAlt2 />
                 </button>
